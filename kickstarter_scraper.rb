f5b6e5a9389785c6f1dda7d266c06871a6a31f6a
Robert Salaiz <work.salaizrm@gmail.com>
2020-03-02 03:37:20 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,14 +2,17 @@ require 'nokogiri'
 require 'pry'
  
 def create_project_hash
+    projects = {}
     html = 
   File.read('fixtures/kickstarter.html')
     kickstarter = Nokogiri::HTML(html)
     
-    projects = {}
-    
     kickstarter.css("li.project.grid_4").each do |project|
-      proejcts[project] = {}
+      title = project.css("h2.bbcard_name strong a").text
+      
+      projects[title.to_sym] = {:image_link =>
+        
+      }
   end
   projects 
 end
